d60b7539d9324dec5a067ca19f9cbee415a68153
Mohamed Mostafa Moharam <haramm058@gmail.com>
2024-02-22 14:46:09 +0200

    assinments-todolist-childdd  13

--- src/components/Parent/child/ChildClasses.jsx
+++ src/components/Parent/child/ChildClasses.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import Teacher from '../../../assets/connect-teatcher (1).png';
 const ChildClasses = () => {
   return (
-    <div>ChildClasses</div>
+    <div className='flex flex-col py-3 px-4 w-full'>
+      <div className="bg-gray-100 rounded-lg between p-3 ">
+        <div className="flex gap-3">
+          <img src={Teacher} alt="Teacher"className='w-14 h-14' />
+          <div className="flex flex-col">
+          <h4 class="font-semibold text-lg leading-9 font-poppins">Arabic</h4>
+          <p class="font-poppins font-normal text-sm leading-6">Guy Hawkins</p>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
